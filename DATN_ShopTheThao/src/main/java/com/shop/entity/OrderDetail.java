34a346e720a29bb226395a13132321158bf571a0
hoangminhanh3221 <hoangminhanh3221@gmail.com>
2023-06-05 13:49:30 +0700

    Update OrderDetail.java

--- DATN_ShopTheThao/src/main/java/com/shop/entity/OrderDetail.java
+++ DATN_ShopTheThao/src/main/java/com/shop/entity/OrderDetail.java
@@ -1,5 +1,20 @@
 package com.shop.entity;
 
-public class OrderDetail {
+import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ORDERDETAIL")
+public class OrderDetail implements Serializable{
+	private Integer orderDetailId;
+	private 
 }
