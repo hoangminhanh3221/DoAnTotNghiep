f86852965c94f541b16406163d0f0394a45726d9
hoangminhanh3221 <hoangminhanh3221@gmail.com>
2023-06-13 11:53:21 +0700

    ok

---
+++ DATN_ShopTheThao/src/main/java/com/shop/service/FavoriteService.java
@@ -0,0 +1,23 @@
+package com.shop.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.shop.entity.Favorite;
+
+public interface FavoriteService{
+	List<Favorite> findAllFavorites();
+	
+	Page<Favorite> findAllFavorites(Pageable pageable);
+	
+	Optional<Favorite> findFavoriteById(Integer favoriteId);
+	
+	Favorite createFavorite(Favorite favorite);
+	
+	Favorite updateFavorite(Favorite favorite);
+	
+	void deleteFavorite(Integer favoriterId);
+}
---
+++ DATN_ShopTheThao/src/main/java/com/shop/service/FeedbackService.java
@@ -0,0 +1,23 @@
+package com.shop.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import com.shop.entity.Feedback;
+
+public interface FeedbackService{
+	List<Feedback> findAllFeedbacks();
+	
+	Page<Feedback> findAllFeedbacks(Pageable pageable);
+	
+	Optional<Feedback> findFeedbackById(Integer feedbackId);
+	
+	Feedback createFeedback(Feedback feedback);
+	
+	Feedback updateFeedback(Feedback feedback);
+	
+	void deleteFeedback(Integer feedbackId);
+}
