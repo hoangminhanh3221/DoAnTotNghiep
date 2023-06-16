1e540bace685bff1421fde419c57121b891238f9
hoangminhanh3221 <hoangminhanh3221@gmail.com>
2023-06-16 13:43:30 +0700

    ok

--- DATN_ShopTheThao/src/main/java/com/shop/controller/ProductController.java
+++ DATN_ShopTheThao/src/main/java/com/shop/controller/ProductController.java
@@ -1,9 +1,13 @@
 package com.shop.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.shop.entity.Product;
 import com.shop.service.ProductService;
 
 @Controller
@@ -17,8 +21,10 @@ public class ProductController {
 		this.productService = productService;
 	}
 	
-	@RequestMapping("/list-product")
-	public String getProduct() {
+	@RequestMapping("/list-all-product")
+	public String getProduct(Model model) {
+		List<Product> lisProducts = productService.findAllProduct();
+		model.addAttribute("allProduct", lisProducts);
 		return "user-page/product";
 	}
 	
