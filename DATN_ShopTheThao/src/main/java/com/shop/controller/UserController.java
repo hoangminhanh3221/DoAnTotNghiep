ef964ec47243d848ccd5fb0f842b07ee3fbf1300
hoangminhanh3221 <hoangminhanh3221@gmail.com>
2023-06-20 11:42:59 +0700

    ok

--- DATN_ShopTheThao/src/main/java/com/shop/controller/UserController.java
+++ DATN_ShopTheThao/src/main/java/com/shop/controller/UserController.java
@@ -1,17 +1,27 @@
 package com.shop.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.shop.entity.Account;
+import com.shop.service.AccountService;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+	
+	@Autowired
+	private AccountService accountService;
+	
 	@RequestMapping("/info")
 	public String getInfo() {
+		Account account = accountService.findAccountById("kiet").orElse(null);
 		return "user-page/user-info";
 	}
 	@RequestMapping("/list-order")
 	public String getListOrder() {
+		Account account = accountService.findAccountById("kiet").orElse(null);
 		return "user-page/list-order";
 	}
 }
