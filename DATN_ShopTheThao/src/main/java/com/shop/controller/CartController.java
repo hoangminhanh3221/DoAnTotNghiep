248f1c2af23e59ff20a6e9f364dcc9cf65d15e0f
hoangminhanh3221 <hoangminhanh3221@gmail.com>
2023-06-24 13:32:02 +0700

    Create CartController.java

---
+++ DATN_ShopTheThao/src/main/java/com/shop/controller/CartController.java
@@ -0,0 +1,27 @@
+package com.shop.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.shop.entity.Order;
+import com.shop.service.OrderService;
+import com.shop.service.ProductService;
+
+@Controller
+public class CartController {
+	
+	@Autowired
+	private final ProductService productService;
+	
+	@Autowired
+	private final OrderService orderService;
+	
+	@RequestMapping("/cart/add")
+	public String getAddCart(Model model) {
+		return null;
+		
+	}
+	
+}
