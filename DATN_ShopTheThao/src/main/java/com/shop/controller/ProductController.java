701e1975e257a31c6be0b730b2796a553b1cba5e
hoangminhanh3221 <hoangminhanh3221@gmail.com>
2023-06-30 08:45:42 +0700

    doing

--- DATN_ShopTheThao/src/main/java/com/shop/controller/ProductController.java
+++ DATN_ShopTheThao/src/main/java/com/shop/controller/ProductController.java
@@ -1,6 +1,7 @@
 package com.shop.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -31,9 +32,17 @@ public class ProductController {
 	
 	@RequestMapping("/product-detail/{id}")
 	public String getProductDetail(@PathVariable("id") String maSP, Model model) {
-		Product product = productService.findProductById(maSP).orElse(null);
-		model.addAttribute("product", product);
-		return "user-page/product-detail";
+	    Optional<Product> optionalProduct = productService.findProductById(maSP);
+	    System.out.println(maSP);
+	    if (optionalProduct.isPresent()) {
+	        Product product = optionalProduct.get();
+	        System.out.println(product.getProductName());
+	        model.addAttribute("product", product);
+	        return "user-page/product-detail";
+	    } else {
+	        System.out.println("Không tìm thấy product");
+	        return "redirect:/product/list-all-product";
+	    }
 	}
 	
 }
