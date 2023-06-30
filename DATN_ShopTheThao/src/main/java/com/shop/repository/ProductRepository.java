3df338853f0dbb43c68feccf8b3efe406e1740bc
hoangminhanh3221 <hoangminhanh3221@gmail.com>
2023-06-30 20:51:30 +0700

    doing

--- DATN_ShopTheThao/src/main/java/com/shop/repository/ProductRepository.java
+++ DATN_ShopTheThao/src/main/java/com/shop/repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.shop.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.shop.entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product, String>{
-
+	List<Product> findBySubcategory_Category_CategoryId(String categoryId);
 }
