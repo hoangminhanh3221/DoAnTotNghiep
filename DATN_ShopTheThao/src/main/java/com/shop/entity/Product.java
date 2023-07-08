fc2e8473c4494d0697014840e9844d17f7fae780
hoangminhanh3221 <hoangminhanh3221@gmail.com>
2023-07-08 15:10:30 +0700

    Revert "dangnhap"
    This reverts commit 49863b3b09784242d449331db67ebd2410bac063.

--- DATN_ShopTheThao/src/main/java/com/shop/entity/Product.java
+++ DATN_ShopTheThao/src/main/java/com/shop/entity/Product.java
@@ -94,10 +94,5 @@ public class Product implements Serializable{
 	@JsonIgnore
 	@OneToMany(mappedBy = "product", cascade = CascadeType.ALL)
 	private List<Feedback> feedbacks;
-
-	public char[] getProductName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 	 
 }
