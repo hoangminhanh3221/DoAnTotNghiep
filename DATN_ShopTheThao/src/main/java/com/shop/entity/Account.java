7dfa063722add072dacfd4a6d30a10ba12ad31ff
MinhTall <dangminhquan9a5@gmail.com>
2023-07-15 14:31:01 +0700

    get/setter account&product

--- DATN_ShopTheThao/src/main/java/com/shop/entity/Account.java
+++ DATN_ShopTheThao/src/main/java/com/shop/entity/Account.java
@@ -55,4 +55,70 @@ public class Account implements Serializable {
 	@OneToMany(mappedBy = "account", cascade = CascadeType.ALL)
 	private List<Order> orders;
 
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public List<Customer> getCustomers() {
+		return customers;
+	}
+
+	public void setCustomers(List<Customer> customers) {
+		this.customers = customers;
+	}
+
+	public List<Employee> getEmployees() {
+		return employees;
+	}
+
+	public void setEmployees(List<Employee> employees) {
+		this.employees = employees;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
+	}
+
+	public void setOrders(List<Order> orders) {
+		this.orders = orders;
+	}
+	
+	
+	
 }
