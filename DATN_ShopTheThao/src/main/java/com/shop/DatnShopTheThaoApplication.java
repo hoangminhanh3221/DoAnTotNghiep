f25b706ff1b869c59f164200172a08ce28738468
hoangminhanh3221 <hoangminhanh3221@gmail.com>
2023-07-17 13:41:14 +0700

    doing

--- DATN_ShopTheThao/src/main/java/com/shop/DatnShopTheThaoApplication.java
+++ DATN_ShopTheThao/src/main/java/com/shop/DatnShopTheThaoApplication.java
@@ -1,5 +1,7 @@
 package com.shop;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,6 +12,12 @@ public class DatnShopTheThaoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DatnShopTheThaoApplication.class, args);
+//		Runtime rt = Runtime.getRuntime();
+//		try {
+//		    rt.exec("cmd /c start msedge http://localhost:8080/home");
+//		} catch (IOException e) {
+//		    e.printStackTrace();
+//		}
 	}
 
 }
---
+++ DATN_ShopTheThao/src/main/java/com/shop/controller/SecurityController.java
@@ -0,0 +1,61 @@
+package com.shop.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.shop.util.AuthenticationFacade;
+
+@Controller
+public class SecurityController {
+	
+	@Autowired
+    private AuthenticationFacade authenticationFacade;
+	
+	@RequestMapping("/account/login/form")
+	public String loginForm(Model model) {
+		return "/account/login";
+	}
+	
+	@RequestMapping("/account/forgot-password")
+	public String forgotPass(Model model) {
+		return "/account/forgot-password";
+	}
+	
+	@RequestMapping("/account/login/success")
+	public String loginSuccess(Model model) {
+		if(authenticationFacade.hasRole("ROLE_user")) {
+			System.out.println("1");
+			return "redirect:/home";
+		}
+		System.out.println("2");
+		return "/admin-page/dashboard";
+	}
+	
+	@RequestMapping("/account/login/error")
+	public String loginError(Model model) {
+		return "/account/login";
+	}
+	
+	@RequestMapping("/user-page/home")
+	public String homeLoad(Model model) {
+		return "/user-page/home";
+	}
+	
+	@RequestMapping("/account/unauthoried")
+	public String unauthoried(Model model) {
+		return "/account/login";
+	}
+	
+	@RequestMapping("/account/logoff/success")
+	public String logoffSuccess(Model model) {
+		return "/account/login";
+	}
+	
+	@RequestMapping("/account/register")
+	public String accRegister(Model model) {
+		return "/account/register";
+	}
+	
+}
