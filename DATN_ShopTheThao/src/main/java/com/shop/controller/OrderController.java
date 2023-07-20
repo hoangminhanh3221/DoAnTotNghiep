0bfa9982f65891135b0df9003974353d8de1ac30
MinhTall <dangminhquan9a5@gmail.com>
2023-07-20 14:03:46 +0700

    ok

--- DATN_ShopTheThao/src/main/java/com/shop/controller/OrderController.java
+++ DATN_ShopTheThao/src/main/java/com/shop/controller/OrderController.java
@@ -13,4 +13,8 @@ public class OrderController {
 	public String list() {
 		return "order/list";
 	}
+	@RequestMapping("/admin/order")
+	public String admorder() {
+		return "admin-page/order-list";
+	}
 }
