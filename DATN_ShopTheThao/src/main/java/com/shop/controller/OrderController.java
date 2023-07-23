0aa02f761f8b87bec27c7e42b8c223d89e2b327e
MinhTall <dangminhquan9a5@gmail.com>
2023-07-23 13:21:38 +0700

    doing order

--- DATN_ShopTheThao/src/main/java/com/shop/controller/OrderController.java
+++ DATN_ShopTheThao/src/main/java/com/shop/controller/OrderController.java
@@ -1,5 +1,6 @@
 package com.shop.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.shop.entity.Order;
+import com.shop.entity.OrderDetail;
+import com.shop.service.OrderDetailService;
 import com.shop.service.OrderService;
 
 @Controller
@@ -18,17 +21,27 @@ public class OrderController {
 	@Autowired
 	private OrderService orderService;
 	
+	@Autowired
+	private OrderDetailService orderDetailService;
+	
 	@RequestMapping("/admin/order")
-	public String admorder() {
-		
-		return "admin-page/order-list";
+	public String admorder(Model model) {
+		 List<Order> orders = orderService.findAllOrders();
+	        System.out.println(orders.get(0).getOrderId());
+	        orders.get(0);
+	        List<Double> listTP = new ArrayList<>();
+	        double totalPrice =0;
+	        for (Order order : orders) {
+				for (OrderDetail orderdetail : order.getOrderDetails()) {
+					totalPrice = totalPrice + orderdetail.getProductPrice();
+				}
+				listTP.add(totalPrice);
+				totalPrice= 0;
+			}
+	        model.addAttribute("orders", orders);
+	        model.addAttribute("total", listTP);
+	        return "admin-page/order-list"; // Trả về tên của trang HTML muốn hiển thị (ví dụ: orders.html)
 	}
 	
-	@GetMapping("/orders")
-    public String getAllOrders(Model model) {
-        List<Order> orders = orderService.findAllOrders();
-        System.out.println(orders.get(0).get);
-        model.addAttribute("orders", orders);
-        return "admin-page/order-list"; // Trả về tên của trang HTML muốn hiển thị (ví dụ: orders.html)
-    }
+
 }
